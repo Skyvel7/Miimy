4c35861d738bec77dff461ecbc9e3b9253dd834a
Skyvel7 <59847574+Skyvel7@users.noreply.github.com>
2025-09-25 13:30:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const openLetterBtn = document.getElementById('open-letter-btn');
     const initialContent = document.querySelector('.initial-content');
     const letterContent = document.getElementById('letter-content');
+    const birthdaySong = document.getElementById('birthday-song');
+
+    // Variable para asegurar que la música solo se inicie una vez
+    let isMusicPlaying = false;
 
     openLetterBtn.addEventListener('click', () => {
+        // Inicia la música si aún no está sonando
+        if (!isMusicPlaying) {
+            birthdaySong.play();
+            isMusicPlaying = true;
+        }
+
         // Oculta el contenido inicial
         initialContent.style.display = 'none';
 
